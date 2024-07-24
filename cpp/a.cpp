4fbffeb24e23606122348c3f392b178b621c65a8
qian488 <3517576561@qq.com>
2024-07-24 08:47:40 +0800

    create a.cpp

---
+++ cpp/a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+#define endl "\n"
+using namespace std;
+
+void Solve(void){
+    
+}
+
+int main(){
+    int t=1;
+    cin>>t;
+    while (t--)
+    {
+        Solve();
+    }
+    return 0;
+}
